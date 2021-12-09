9c0acadcf8d874389dd12346f4771482e077ee6d
RuslanPanasuk <panasuk.eetp@gmail.com>
2021-12-09 09:28:34 +0200

    init: stert human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function toReadable (number) {
-  
+  const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 }
